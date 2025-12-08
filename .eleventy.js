94e6406db4626b18c027e09cac7979556dc7fec9
jas <cchenzyy@icloud.com>
2025-12-08 17:15:16 +0800

    本地更改

--- .eleventy.js
+++ .eleventy.js
@@ -529,7 +529,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
-  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/site/fonts");
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
